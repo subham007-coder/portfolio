aa8517ad3389238ff0efd9bb63a6a8e1e7027a05
subham007-coder <dsubham109@gmail.com>
2024-01-30 22:23:21 +0530

    add

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let car = document.querySelector(".car");
 let htmlLogo = document.querySelector(".htmllogo");
 let headingHtml = document.querySelector(".heding1");
 let innerText = document.querySelector(".innertext1");
 
 
+// gsap.to(".car", {
+//     x: 120,
+//     duration: 1
+// });
+
 document.onkeydown = ((e) => {
-    console.log("hii", e.code);
-    if (e.code == "ArrowRight") {
-        console.log("helow");
-        car.style.left = "-17px"
-    } if (car.style.left = "-17px") {
+    console.log("hii", e.keyCode);
+    if (e.keyCode == 39) {
+        let car = document.querySelector(".car");
+        let carX = parseInt(window.getComputedStyle(car, null).getPropertyValue('left'));
+        car.style.left = carX + 112+"px";
         htmlLogo.classList.remove("none");
         headingHtml.classList.remove("none");
         innerText.classList.remove("none");
