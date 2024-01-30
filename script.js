9bc76197c9cacf43138a613ee81a6bbf9e42bccc
subham007-coder <dsubham109@gmail.com>
2024-01-30 21:09:18 +0530

    add

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let car = document.querySelector(".car");
+let htmlLogo = document.querySelector(".htmllogo");
+let headingHtml = document.querySelector(".heding1");
+let innerText = document.querySelector(".innertext1");
+
 
 document.onkeydown = ((e) => {
     console.log("hii", e.code);
     if (e.code == "ArrowRight") {
         console.log("helow");
         car.style.left = "-17px"
+    } if (car.style.left = "-17px") {
+        htmlLogo.classList.remove("none");
+        headingHtml.classList.remove("none");
+        innerText.classList.remove("none");
     }
 });
